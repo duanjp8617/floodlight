842cf35a8e92ccf8ff595fc8a9d5f1d5e1012ddc
ly <djp@ubuntu.ubuntu-domain>
2015-07-01 16:36:17 +0800

    another commit

--- src/main/java/net/floodlightcontroller/nfvtest/MasterServiceChain.java
+++ src/main/java/net/floodlightcontroller/nfvtest/MasterServiceChain.java
@@ -6,33 +6,37 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.Map;
 
 public class MasterServiceChain implements Runnable{
-	private final Map<String, ConcurrentLinkedQueue<Message>> queueMap;
-	private final Map<String, IQueueProcessor> processorMap;
+	private final Map<String, ConcurrentLinkedQueue<Message>> messageQueueMap;
+	private final Map<String, SlaveServiceChain> slaveMap;
 	private final NFVServiceChainStorage serviceChainStorage;
 	
 	private boolean exitFlag;
 	
 	MasterServiceChain(NFVServiceChainStorage s) {
 		this.serviceChainStorage = s;
-		queueMap = new HashMap<String, ConcurrentLinkedQueue<Message>>();
-		processorMap = new HashMap<String, IQueueProcessor>();
+		messageQueueMap = new HashMap<String, ConcurrentLinkedQueue<Message>>();
+		slaveMap = new HashMap<String, SlaveServiceChain>();
 		exitFlag = false;
 	}
 	
-	public ConcurrentLinkedQueue<Message> register(String id, IQueueProcessor queueProcessor){
-		ConcurrentLinkedQueue<Message> q = new ConcurrentLinkedQueue<Message>();
-		queueMap.put(id, q);
-		processorMap.put(id, queueProcessor);
-		return q;
+	public void register(SlaveServiceChain slave){
+		slaveMap.put(slave.getId(), slave);
+		ConcurrentLinkedQueue<Message> messageQueue = new ConcurrentLinkedQueue<Message>();
+		messageQueueMap.put(slave.getId(), messageQueue);
+		slave.setMaster(this);
+	}
+	
+	public ConcurrentLinkedQueue<Message> getMessageQueueForSlave(String id){
+		return messageQueueMap.get(id);
 	}
 
 	@Override
 	public void run() {
 		while(!getExitFlag()){
-			for(String key : queueMap.keySet()){
-				ConcurrentLinkedQueue<Message> queue = queueMap.get(key);
-				IQueueProcessor processor = processorMap.get(key);
-				processor.processQueue(queue);
+			for(String key : messageQueueMap.keySet()){
+				ConcurrentLinkedQueue<Message> messageQueue = messageQueueMap.get(key);
+				SlaveServiceChain slave = slaveMap.get(key);
+				slave.processMessagesSentToMaster(messageQueue);
 			}
 			//Then add some method for the NFVServiceChainStorage.s
 			pollFlowStatusFromStorage();
