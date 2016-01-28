099ff0f5d53fc91b482566f284e42729c6a891c2
djp <dj8617@djps-MacBook-Pro.local>
2016-01-29 02:20:59 +0800

    enable reactive scaling

--- src/main/java/net/floodlightcontroller/nfvtest/nfvcorestructure/NFVTest.java
+++ src/main/java/net/floodlightcontroller/nfvtest/nfvcorestructure/NFVTest.java
@@ -176,9 +176,9 @@ public class NFVTest implements IOFMessageListener, IFloodlightModule {
 		ServiceChainConfig cpServiceChainConfig = new ServiceChainConfig("CONTROL", 2, cpList);
 		
 		//create data plane service chain configuration
-		StageVmInfo firewallInfo = new StageVmInfo(2, 2*1024, 2*1024, "firewall_mini.img", 90, -1, 50000);
-		StageVmInfo ipsInfo = new StageVmInfo(2, 2*1024, 2*1024, "snort_mini.img", 90, -1, 24000);
-		StageVmInfo transcoderInfo = new StageVmInfo(2, 2*1024, 2*1024, "transcoder_mini.img", 90, -1, 13000);
+		StageVmInfo firewallInfo = new StageVmInfo(2, 2*1024, 2*1024, "firewall_mini.img", 45, -1, 50000);
+		StageVmInfo ipsInfo = new StageVmInfo(2, 2*1024, 2*1024, "snort_mini.img", 45, -1, 20000);
+		StageVmInfo transcoderInfo = new StageVmInfo(2, 2*1024, 2*1024, "transcoder_mini.img", 45, -1, 13000);
 		ArrayList<StageVmInfo> dpList = new ArrayList<StageVmInfo>();
 		dpList.add(firewallInfo);
 		dpList.add(ipsInfo);
