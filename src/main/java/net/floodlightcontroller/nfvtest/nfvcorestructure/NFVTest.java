0dac18ec980d81a71ba71624841cfeb2fc31fcd0
djp <dj8617@djps-MacBook-Pro.local>
2016-01-29 11:33:23 +0800

    debug

--- src/main/java/net/floodlightcontroller/nfvtest/nfvcorestructure/NFVTest.java
+++ src/main/java/net/floodlightcontroller/nfvtest/nfvcorestructure/NFVTest.java
@@ -177,7 +177,7 @@ public class NFVTest implements IOFMessageListener, IFloodlightModule {
 		
 		//create data plane service chain configuration
 		StageVmInfo firewallInfo = new StageVmInfo(2, 2*1024, 2*1024, "firewall_mini_v2.img", 45, -1, 50000);
-		StageVmInfo ipsInfo = new StageVmInfo(2, 2*1024, 2*1024, "snort_mini.img_v2", 45, -1, 20000);
+		StageVmInfo ipsInfo = new StageVmInfo(2, 2*1024, 2*1024, "snort_mini_v2.img", 45, -1, 20000);
 		StageVmInfo transcoderInfo = new StageVmInfo(2, 2*1024, 2*1024, "transcoder_mini_v2.img", 45, -1, 13000);
 		ArrayList<StageVmInfo> dpList = new ArrayList<StageVmInfo>();
 		dpList.add(firewallInfo);
