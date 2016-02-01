e33ffeefa84409e1ce8496675472bf09daab8b74
djp <dj8617@djps-MacBook-Pro.local>
2016-02-01 18:51:25 +0800

    disable real vm creation

--- src/main/java/net/floodlightcontroller/nfvtest/nfvslaveservice/ServiceChainHandler.java
+++ src/main/java/net/floodlightcontroller/nfvtest/nfvslaveservice/ServiceChainHandler.java
@@ -335,9 +335,9 @@ public class ServiceChainHandler extends MessageProcessor {
 									domainName = "sprout.cw.t";
 								}
 								
-								DNSUpdateRequest dnsUpdateReq = new DNSUpdateRequest(this.getId(), domainName, 
+								/*DNSUpdateRequest dnsUpdateReq = new DNSUpdateRequest(this.getId(), domainName, 
 										workingNode.vmInstance.operationIp, "delete");
-								this.mh.sendTo("dnsUpdator", dnsUpdateReq);
+								this.mh.sendTo("dnsUpdator", dnsUpdateReq);*/
 							}
 						}
 					}
