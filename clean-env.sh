9409641ccc42642dce40de270fda5c0b9f9844e4
djp <duanjp8617@gmail.com>
2016-04-10 17:37:18 +0800

    revert real vm creation

--- clean-env.sh
+++ clean-env.sh
@@ -3,13 +3,13 @@ sudo ovs-vsctl del-br DATA-br0
 sudo ovs-vsctl del-br DATA-br1
 sudo ovs-vsctl del-br DATA-br2
 sudo ovs-vsctl del-br DATA-br3
-#virsh net-destroy DATA-m
-#virsh net-destroy CONTROL-m
-#virsh net-destroy CONTROL-o
-#virsh net-destroy TEST-0
-#virsh net-destroy TEST-1
-#virsh net-destroy TEST-2
-#virsh net-destroy TEST-3
-#rm -f ~/nfvenv/xml/*
-#sudo rm -f ~/nfvenv/img/DATA*
-#sudo rm -f ~/nfvenv/img/CONTROL*
+virsh net-destroy DATA-m
+virsh net-destroy CONTROL-m
+virsh net-destroy CONTROL-o
+virsh net-destroy TEST-0
+virsh net-destroy TEST-1
+virsh net-destroy TEST-2
+virsh net-destroy TEST-3
+rm -f ~/nfvenv/xml/*
+sudo rm -f ~/nfvenv/img/DATA*
+sudo rm -f ~/nfvenv/img/CONTROL*
--- src/main/java/net/floodlightcontroller/nfvtest/nfvslaveservice/SubscriberConnector.java
+++ src/main/java/net/floodlightcontroller/nfvtest/nfvslaveservice/SubscriberConnector.java
@@ -116,12 +116,12 @@ public class SubscriberConnector extends MessageProcessor{
 	}
 	
 	private void subscriberConnect(SubConnRequest request){
-		/*SubConnThread newThread = new SubConnThread(request, this.zmqContext, this.mh, this.logger);
+		SubConnThread newThread = new SubConnThread(request, this.zmqContext, this.mh, this.logger);
 		Thread t = new Thread(newThread);
-		t.start();*/
-		SubConnReply reply = new SubConnReply("hehe", request, null, null);
-		mh.sendTo(request.getSourceId(), reply);
-		return;
+		t.start();
+		//SubConnReply reply = new SubConnReply("hehe", request, null, null);
+		//mh.sendTo(request.getSourceId(), reply);
+		//return;
 	}
 	
 }
